1dda568c729a2986e4fb48fabbcc41401b3348f9
tkdhanasekar <tkdhanasekar@gmail.com>
2023-12-09 14:46:10 +0530

    version updation

--- jenkins-install/jenkins-ubuntu-installation.sh
+++ jenkins-install/jenkins-ubuntu-installation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo apt install openjdk-18-jre -y
+sudo apt install openjdk-17-jre -y
 sudo mkdir -p /usr/share/keyrings
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
