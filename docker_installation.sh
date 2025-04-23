9de7e0b4b6dad4acf3d58a492e54da70c567e0c6
tkdhanasekar <tkdhanasekar@gmail.com>
2025-04-23 14:02:49 +0530

    docker installation script

--- docker_installation.sh
+++ docker_installation.sh
@@ -2,17 +2,16 @@ sudo apt update
 sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-curl -L "https://github.com/docker/compose/releases/download/v2.35.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.35.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 ls -lsh /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-systemctl start docker && systemctl enable docker
-chmod +x /usr/bin/docker-compose
-docker version && docker compose version && docker-compose version
-sudo groupadd docker
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker && sudo systemctl enable docker
+sudo chmod +x /usr/bin/docker-compose
 sudo usermod -aG docker $USER
 newgrp docker
+docker version && docker compose version && docker-compose version
